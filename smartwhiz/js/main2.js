5c8f7340f9ba77aa78f8f910e7545466d847a5cb
saabirmd7 <saabirmd7@gmail.com>
2025-09-08 08:15:18 +0000

    feat: Apply mobile view fixes

--- smartwhiz/js/main2.js
+++ smartwhiz/js/main2.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li a');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+            }
+        });
     });
 
     const courseReadMoreBtns = document.querySelectorAll('.course-read-more-btn');
